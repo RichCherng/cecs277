b6840f00fb180906ddf669683f4bae77868f3df7
Rich Cherngchaosil <richer_po@hotmail.com>
2015-03-09 14:05:34 -0700

    Implement Beginner vs Veteran

--- src/Assignment3/Computer.java
+++ src/Assignment3/Computer.java
@@ -6,42 +6,59 @@ import java.util.Random;
 
 public class Computer implements Serializable {
 
-	private HashMap<Pattern,Integer> hash;
-	public Computer(){
-		hash = new HashMap<Pattern,Integer>();
+	private HashMap<Pattern, Integer> hash;
+
+	public Computer() {
+		hash = new HashMap<Pattern, Integer>();
 	}
-	
-	public void add(Pattern p){
-		if(hash.containsKey(p))
+
+	public void add(Pattern p) {
+		if (hash.containsKey(p))
 			hash.put(p, hash.get(p) + 1);
 		else
 			hash.put(p, 1);
-		
-		for(Pattern i: hash.keySet()){
-			System.out.println(i.getPattern());
-		}
+
+		/*
+		 * for(Pattern i: hash.keySet()){ System.out.println(i.getPattern()); }
+		 */
 	}
-	
-	public char prediction(String pattern){
-		String p = pattern.substring(1, pattern.length()), predict = null;
-		int max = 0;
-		char[] m = {'r','p','s'};
-		for(int i = 0; i < m.length;i++){
-			if(hash.containsKey(new Pattern(p + m[i]))){
-				if (max < hash.get(new Pattern(p + m[i]))){
-					max = hash.get(new Pattern(p + m[i]));
-					predict = p + m[i];
+
+	public char prediction(String pattern) {
+		if (pattern.length() != 4) {
+			System.out.println(pattern);
+			return randPick();
+
+		} else {
+			String p = pattern.substring(1, pattern.length()), predict = null;
+			int max = 0;
+			char[] m = { 'r', 'p', 's' };
+			for (int i = 0; i < m.length; i++) {
+				if (hash.containsKey(new Pattern(p + m[i]))) {
+					if (max < hash.get(new Pattern(p + m[i]))) {
+						max = hash.get(new Pattern(p + m[i]));
+						predict = p + m[i];
+					}
 				}
 			}
+			if (predict != null) {
+				char cPredict = predict.charAt(predict.length() - 1);
+				if (cPredict == 'r')
+					return 'p';
+				else if (cPredict == 'p')
+					return 's';
+				else if (cPredict == 's')
+					return 'p';
+			}
+
 		}
-		if(predict == null){
-			Random rd = new Random();
-			System.out.println("test");
-			return m[rd.nextInt(m.length)];
-		}
-		else
-		{
-			return predict.charAt(predict.length() - 1);
-		}
+		return randPick();
+
+	}
+
+	public char randPick() {
+		char[] m = { 'r', 'p', 's' };
+		Random rd = new Random();
+		// System.out.println("test");
+		return m[rd.nextInt(m.length)];
 	}
 }
--- src/Assignment3/Main.java
+++ src/Assignment3/Main.java
@@ -1,28 +1,98 @@
 package Assignment3;
 
-import java.util.Random;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Choose R - Rock\nChoose P - Paper\nChoose S - Scissor\n");
+		File file = new File("computer.dat");
 		Computer computer = new Computer();
 		String pattern = "";
-		while(pattern.length() < 4){
+		int choice  = 1;
+		char[] m = {'r','p','s'};
+		boolean running = true;
+		
+		//Game choices for Veteran or Beginner
+		System.out.println("Choose game mode\n1.Beginner\n2.Veteran\n");
+		choice = checkInt(1,2);
+		//if choice is veteran then load file, else do nothing.
+		if(choice == 2){
+			//Process of checking and reading file for existing data.
+			if(file.exists()){
+				try{
+					ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+					try{
+						computer = (Computer) in.readObject();
+					} catch(ClassNotFoundException e){
+						e.printStackTrace();
+					}
+					in.close();
+				} catch(FileNotFoundException e){
+					e.printStackTrace();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		} 
+		
+		/*while(pattern.length() < 4 && running){
 			//play first 5 round first
-			System.out.print("Choose your Move:");
-			pattern += scanner.nextLine();
+			System.out.print("Choose your Move:\n1.Rock\n2.Paper\n3.Scissor\n4.Quit\n");
+			choice = checkInt(1,4);
+			if(choice == 4){
+				running = false;
+				break;
+			}
+			pattern += m[choice - 1];
+			System.out.println("Computer choose: " + computer.randPick());
 		}
-		System.out.println(pattern);
+		*/
+		
+		
+		//System.out.println(pattern);
 		computer.add(new Pattern(pattern));
-		while(true){
-			System.out.print("Choose your Move:");
+		while(running){
+			System.out.print("Choose your Move:\n1.Rock\n2.Paper\n3.Scissor\n4.Quit\n");
+			choice = checkInt(1,4);
+			if(choice == 4){
+				break;
+			}
 			char predict = computer.prediction(pattern);
-			pattern = pattern.substring(1) + scanner.nextLine();
+			if(pattern.length() == 4)
+				pattern = pattern.substring(1) + m[choice - 1];
+			else
+				pattern += m[choice - 1];
 			System.out.println("Computer choose: " + predict);
 			computer.add(new Pattern(pattern));
 		}
 	}
+	
+	public static int checkInt(int low, int high){
+		Scanner in = new Scanner(System.in);
+		boolean valid = false;
+		int validNum = 0;
+		
+		while(!valid){
+			
+			if(in.hasNextInt()){
+				validNum = in.nextInt();
+				if(validNum >= low && validNum <= high){
+					valid = true;
+				}else{
+					System.out.print("Invalid- Retry: ");
+				}
+			}else {
+				in.next();
+				System.out.println("Invalid input- Retry: ");
+			}
+		}
+		return validNum;
+	}
+	
 }
